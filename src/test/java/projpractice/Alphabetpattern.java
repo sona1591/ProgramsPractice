4ee43ed32e82f608a9c85c04a5b9b49512add900
sonal <sonalagrawal1506@gmail.com>
2024-03-01 15:15:39 +0530

    added comment

--- src/test/java/projpractice/Alphabetpattern.java
+++ src/test/java/projpractice/Alphabetpattern.java
@@ -3,7 +3,8 @@ package projpractice;
 public class Alphabetpattern {
 
 	public static void main(String[] args) {
-		int alpha=65;
+		int alpha=65;//A
+		//int alpha=97;//a
 		
 		for(int i=0;i<=5;i++) {
 			for (int j=0;j<=i;j++)
