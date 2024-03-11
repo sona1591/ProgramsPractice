3c2c93c403248421d8fbd055f8e33aae70218015
sonal <sonalagrawal1506@gmail.com>
2024-03-11 12:44:43 +0530

    added one more logic

--- src/test/java/projpractice/numberPattern2.java
+++ src/test/java/projpractice/numberPattern2.java
@@ -4,7 +4,8 @@ public class numberPattern2 {
 
 	public static void main(String[] args) {
 		
-		for(int i=1;i<=5;i++)
+		//for(int i=1;i<=5;i++)//o/p: 1 22 333 4444 55555
+		for(int i=5;i>=1;i--)//55555 4444 333 22 1
 		{
 			for(int j=1;j<=i;j++)
 			{
