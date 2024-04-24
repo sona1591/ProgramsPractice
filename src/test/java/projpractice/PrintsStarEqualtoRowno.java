82842ebe3c3b09dcf3e039d1ff9b338eacd9435e
sonal <sonalagrawal1506@gmail.com>
2024-04-24 21:52:53 +0530

    Added new program

---
+++ src/test/java/projpractice/PrintsStarEqualtoRowno.java
@@ -0,0 +1,23 @@
+package projpractice;
+
+import java.util.Scanner;
+
+public class PrintsStarEqualtoRowno {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter row no");
+		int row=sc.nextInt();
+		
+		for (int i=1;i<=row;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println(" ");
+			
+		}
+
+	}
+
+}
